124f6465d31be4593903df3fe97ff47a8660a102
dantr <huynhngdantr@gmail.com>
2023-12-01 23:48:31 +0700

    listing create

---
+++ models/listing.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const listingSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    description: { type: String, require: true },
+    address: { type: String, require: true },
+    regularPrice: { type: Number, require: true },
+    discountPrice: { type: Number, require: true },
+    bathRooms: { type: Number, require: true },
+    bedRooms: { type: Number, require: true },
+    furnished: { type: Boolean, require: true },
+    parking: { type: Boolean, require: true },
+    type: { type: String, require: true },
+    offer: { type: Boolean, require: true },
+    imagUrls: { type: Array, require: true },
+    userRef: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+export default Listing;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 import express from "express";
 import userRoutes from "./user.route.js";
 import authRoutes from "./auth.route.js";
+import listingRoutes from "./listing.route.js";
 const router = express.Router();
 
 // Add other routes as needed
 router.use("/user", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/listing", listingRoutes);
 
 export default router;
