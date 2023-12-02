e901898c9025880ae9da9ea3ac7d37e0480ccf29
dantr <huynhngdantr@gmail.com>
2023-12-02 16:52:34 +0700

    add delete route

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -3,9 +3,11 @@ import Listing from "../models/listing.model.js";
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
-   
+
     return res.status(201).json(listing);
   } catch (error) {
     next(error);
   }
 };
+
+export const deleteListing = async (req, res, next) => {};
